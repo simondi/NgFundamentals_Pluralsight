df3055600c0e35beb9bb89a2780cac18f3f05bc0
Michael Tolfo <mtolfo@hchb.com>
2019-01-03 14:23:55 -0500

    Moved inline html to template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list-component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
   imports: [
