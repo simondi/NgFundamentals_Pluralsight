45478dc832b8d79cb69b1e39b1ed82abfe97cf9b
Michael Tolfo <mtolfo@hchb.com>
2019-01-05 20:59:37 -0500

    Angular Template Syntax

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -3,27 +3,60 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'event-thumbnail',
-  template:
-  `<div class="well hoverwell thumbnail">
-    <h2>{{ event.name }}</h2>
-    <div>Date: {{ event.date }}</div>
-    <div>Time: {{ event.time }}</div>
-    <div>Price:  \${{ event.price }}</div>
-    <div>
-      <span>Location: {{ event.location.address }}</span>
-      <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div [ngStyle]="getStartTimeStyle()"
+        [ngSwitch]="event?.time">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{ event?.price }}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{ event?.location?.address }}</span>
+        <span class="pad-left"
+          >{{ event?.location?.city }}, {{ event?.location?.country }}</span
+        >
+      </div>
+      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
     </div>
-  </div>`,
-  styles: [`
-  .pad-left {margin-left: 10px;}
-  .well div {color: #bbb; }
-  `]
+  `,
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+      .thumbnail {
+        min-height: 210px;
+      }
+      .green {
+        color: #003300 !important ;
+      }
+      .bold {
+        font-weight: bold;
+      }
+    `
+  ]
 })
-
 export class EventThumbnailComponent implements OnInit {
   @Input() event: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() { }
+  ngOnInit() {}
+
+  getStartTimeStyle(): any {
+
+    if (this.event && this.event.time === '8:00 am') {
+      return {color: '#003300', 'font-weight': 'bold'};
+    }
+    return {};
+
+  }
 }
