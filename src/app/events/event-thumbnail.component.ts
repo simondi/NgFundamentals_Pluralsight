38306fc2b0e70f447d38ffdcbd74e0aa9401519b
SimonDi <simondi88@hotmail.com>
2019-11-20 14:53:32 -0800

    Merge all the changes over html and css change

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,48 +1,10 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, Pipe } from '@angular/core';
 
 @Component({
   // tslint:disable-next-line:component-selector
   selector: 'event-thumbnail',
-  template: `
-    <div class="well hoverwell thumbnail">
-      <h2>{{ event?.name }}</h2>
-      <div>Date: {{ event?.date }}</div>
-      <div [ngStyle]="getStartTimeStyle()"
-        [ngSwitch]="event?.time">
-        Time: {{ event?.time }}
-        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
-        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-        <span *ngSwitchDefault>(Normal Start)</span>
-      </div>
-      <div>Price: \${{ event?.price }}</div>
-      <div *ngIf="event?.location">
-        <span>Location: {{ event?.location?.address }}</span>
-        <span class="pad-left"
-          >{{ event?.location?.city }}, {{ event?.location?.country }}</span
-        >
-      </div>
-      <div *ngIf="event?.onlineUrl">Online URL: {{ event?.onlineUrl }}</div>
-    </div>
-  `,
-  styles: [
-    `
-      .pad-left {
-        margin-left: 10px;
-      }
-      .well div {
-        color: #bbb;
-      }
-      .thumbnail {
-        min-height: 210px;
-      }
-      .green {
-        color: #003300 !important ;
-      }
-      .bold {
-        font-weight: bold;
-      }
-    `
-  ]
+  templateUrl: 'event-thumbnail.component.html',
+  styleUrls: [ './event-thumbnail.component.css'  ]
 })
 export class EventThumbnailComponent implements OnInit {
   @Input() event: any;
